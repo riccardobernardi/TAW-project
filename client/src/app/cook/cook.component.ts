4d8accb539b6e3ce0ca9dd01c1e5fdf3d21933f7
riccardobernardi <riccardo.bernardi@rocketmail.com>
2019-05-09 16:47:44 +0200

    adding cooks and events

--- client/src/app/cook/cook.component.ts
+++ client/src/app/cook/cook.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {UserService} from '../user.service';
+import {Router} from '@angular/router';
+import {OrderService} from '../order.service';
 
 @Component({
   selector: 'app-cook',
@@ -6,14 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cook.component.css']
 })
 export class CookComponent implements OnInit {
+  private orders: any;
 
-  constructor() { }
+  constructor(private us: UserService, private router: Router, private order: OrderService  ) { }
 
   ngOnInit() {
-    /*this.get_messages();
-    this.sio.connect().subscribe( (m) => {
-      this.get_messages();
-    });*/
+  }
+
+  public get_orders() {
+    this.order.get().subscribe(
+      ( orders ) => {
+        this.orders = orders;
+
+      } , (err) => {
+
+        // Try to renew the token
+        this.us.renew().subscribe( () => {
+          // Succeeded
+          this.order.get();
+        }, (err2) => {
+          // Error again, we really need to logout
+          this.us.logout();
+        } );
+      }
+    );
   }
 
 }
