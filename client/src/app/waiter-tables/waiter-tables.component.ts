fdfd8fe5eda9c5efd65edebf224a8c0dc081bff0
riccardobernardi <riccardo.bernardi@rocketmail.com>
2019-05-20 12:13:15 +0200

    solved conflicts

--- client/src/app/waiter-tables/waiter-tables.component.ts
+++ client/src/app/waiter-tables/waiter-tables.component.ts
@@ -1,9 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { Table } from '../Table';
 import { TableHttpService } from 'src/app/table-http.service';
 import { UserHttpService } from 'src/app/user-http.service';
 import { TicketHttpService } from '../ticket-http.service';
 import { Ticket } from 'src/app/Ticket';
+import {Table} from '../Table';
 
 @Component({
   selector: 'app-waiter-tables',
@@ -12,23 +12,25 @@ import { Ticket } from 'src/app/Ticket';
 })
 export class WaiterTablesComponent implements OnInit {
 
-  private tables: Table[] = [];
+  private tables : Table[] = [];
 
-  constructor(private table: TableHttpService, private user: UserHttpService, private ticket: TicketHttpService) { }
+  constructor(private table : TableHttpService, private user: UserHttpService, private ticket: TicketHttpService) { }
 
   ngOnInit() {
     this.table.get_tables().toPromise().then((data : Table[]) => {
-      this.tables = data;
-      console.log(this.tables[0].state);
+      this.tables = data
+      console.log(this.tables[0].state)
     }).catch((err) => {
       console.log(err);
     });
   }
 
-  open_ticket(tableToChange: Table) {
-    this.ticket.open_ticket(this.user.get_nick(), tableToChange.num).toPromise().then((data : Ticket) => {
-      tableToChange.state = data._id;
-      return this.table.change_table(tableToChange);
+  open_ticket(table_to_change: Table) {
+    this.ticket.open_ticket(this.user.get_nick(), table_to_change.num).toPromise().then((data : Ticket) => {
+      console.log(data);
+      table_to_change.state = data._id;
+      console.log(table_to_change.state);
+      return this.table.change_table(table_to_change);
       // update del tavolo da rimuovere perchè si deve usare il websocket
     }).then().catch(err => {
       console.log(err);
--- client/src/app/waiter/waiterDashboard.module.ts
+++ client/src/app/waiter/waiterDashboard.module.ts
@@ -6,12 +6,12 @@ import { UserHttpService } from '../user-http.service';
 import { WaiterDashboardComponent } from './waiterDashboard.component';
 import { SocketioService } from '../socketio.service';
 import { OrderHttpService } from '../order-http.service';
-import { ItemHttpService } from "../item-http.service"
+import { ItemHttpService } from '../item-http.service';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { WaiterTablesComponent } from '../waiter-tables/waiter-tables.component'
-import { WaiterRoutingModule } from "./waiter-routing.module";
-import { WaiterServedComponent } from "../waiter-served/waiter-served.component";
-import { WaiterInsertOrdersComponent} from "../waiter-insert-orders/waiter-insert-orders.component";
+import { WaiterTablesComponent } from '../waiter-tables/waiter-tables.component';
+import { WaiterRoutingModule } from './waiter-routing.module';
+import { WaiterServedComponent } from '../waiter-served/waiter-served.component';
+import { WaiterInsertOrdersComponent} from '../waiter-insert-orders/waiter-insert-orders.component';
 import { TableHttpService } from '../table-http.service';
 import { TicketHttpService } from '../ticket-http.service';
 
