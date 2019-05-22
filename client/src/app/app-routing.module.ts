c9a95e8ebb6ff5c78b66aad771eac90366a2c58d
riccardobernardi <riccardo.bernardi@rocketmail.com>
2019-05-22 11:17:33 +0200

    commit

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -16,7 +16,7 @@ import { Paydesk2DashboardComponent } from "./paydesk2/paydesk2Dashboard.compone
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: UserLoginComponent },
-  { path: 'waiter', 
+  { path: 'waiter',
     component: WaiterDashboardComponent,
     children: [
       {
@@ -38,7 +38,7 @@ const routes: Routes = [
     ]
   },
   { path: 'cook', component:  CookComponent},
-  { path: 'paydesk2', 
+  { path: 'paydesk2',
     component: Paydesk2DashboardComponent,
     children: [
       {
@@ -78,6 +78,10 @@ const routes: Routes = [
         path: 'paydesk',
         component: PaydeskComponent
       },
+      {
+        path: 'logout',
+        component: LogoutComponent
+      }
     ]
   },
   { path: 'barman', component:  BarmanComponent},
