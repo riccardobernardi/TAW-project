d7544c2d6106c80cd3bc07231c674269a7ce716a
LorenzoCazzaro <lorenz.cazzaro@gmail.com>
2019-06-04 16:22:22 +0200

    Boh

--- client/src/app/Report.ts
+++ client/src/app/Report.ts
@@ -1,8 +1,22 @@
+import {roles} from "./User";
+
 export interface Report {
     _id? : string,
     date: Date,
     total: number,
     total_orders: {dish: number, beverage: number},
     total_customers: number,
-    average_stay: number
+    average_stay: number,
+    users_reports: {}
+}
+
+export interface WaiterReport {
+    username: string,
+    orders_served: number,
+    customers_server: number
+}
+
+export interface CookerBartenderReport {
+    username: string,
+    items_server: string
 }
